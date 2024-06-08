1f2c2e8fee2a56be971d572f71af73f58f1ec6f7
Carson Strachan <carson.strachan@gmail.com>
2024-06-07 23:46:33 -0600

    Updated unit_tests
    Updated Unit Tests from troubleshooting.

--- unit_tests.py
+++ unit_tests.py
@@ -8,28 +8,24 @@ class TestUVSim(unittest.TestCase):
         self.uvsim = UVSim()
 
     def test_read_success(self):
-        program = [1000, 4300]
+        program = [1001, 4300]
         self.uvsim.load_program(program)
-        sys.stdin = StringIO('1234\n')
         original_stdin = sys.stdin
-        try:
-            self.uvsim.run()
-            self.assertEqual(self.uvsim.memory[0], 1234)
-        finally:
-            sys.stdin = original_stdin
+        sys.stdin = StringIO('1001\n')
+        self.uvsim.run()
+        self.assertEqual(self.uvsim.memory[1], 1001)
+        sys.stdin = original_stdin
 
     def test_write_success(self):
         program = [1100, 4300]
-        self.uvsim.memory[0] = 1234
         self.uvsim.load_program(program)
+        self.uvsim.memory[0] = 1234
         output = StringIO()
         original_stdout = sys.stdout
         sys.stdout = output
-        try:
-            self.uvsim.run()
-            self.assertIn('1234', output.getvalue())
-        finally:
-            sys.stdout = original_stdout
+        self.uvsim.run()
+        self.assertIn('1234', output.getvalue())
+        sys.stdout = original_stdout
 
     def test_load_success(self):
         program = [2000, 4300]
@@ -47,35 +43,29 @@ class TestUVSim(unittest.TestCase):
 
     def test_add_success(self):
         program = [2000, 3001, 4300]
-        self.uvsim.memory[0] = 2000
+        self.uvsim.memory[0] = 1723
         self.uvsim.memory[1] = 3001
         self.uvsim.load_program(program)
         self.uvsim.run()
         self.assertEqual(self.uvsim.accumulator, 5001)
 
     def test_subtract_success(self):
-        program = [2000, 3101, 4300]
-        self.uvsim.memory[0] = 2000
-        self.uvsim.memory[1] = 100
+        program = [1000,1001, 2001, 3100, 4300]
         self.uvsim.load_program(program)
         self.uvsim.run()
-        self.assertEqual(self.uvsim.accumulator, 1900)
+        self.assertEqual(self.uvsim.accumulator, 1)
 
     def test_divide_success(self):
-        program = [2000, 3201, 4300]
-        self.uvsim.memory[0] = 1234
-        self.uvsim.memory[1] = 2
+        program = [1000, 2000, 3200, 4300]
         self.uvsim.load_program(program)
         self.uvsim.run()
-        self.assertEqual(self.uvsim.accumulator, 617)
+        self.assertEqual(self.uvsim.accumulator, 1)
 
     def test_multiply_success(self):
-        program = [2000, 3301, 4300]
-        self.uvsim.memory[0] = 1000
-        self.uvsim.memory[1] = 2
+        program = [1000, 2000, 3300, 4300]
         self.uvsim.load_program(program)
         self.uvsim.run()
-        self.assertEqual(self.uvsim.accumulator, 2000)
+        self.assertEqual(self.uvsim.accumulator, 1000000)
 
     def test_branch_success(self):
         program = [4001, 1101, 4300]
@@ -96,12 +86,9 @@ class TestUVSim(unittest.TestCase):
         output = StringIO()
         original_stdout = sys.stdout
         sys.stdout = output
-
-        try:
-            self.uvsim.run()
-            self.assertIn('9999', output.getvalue())
-        finally:
-            sys.stdout = original_stdout
+        self.uvsim.run()
+        self.assertIn('9999', output.getvalue())
+        sys.stdout = original_stdout
 
     def test_branchzero_success(self):
         program = [2000, 4202, 1102, 4300]
@@ -111,12 +98,9 @@ class TestUVSim(unittest.TestCase):
         output = StringIO()
         original_stdout = sys.stdout
         sys.stdout = output
-
-        try:
-            self.uvsim.run()
-            self.assertIn('9999', output.getvalue())
-        finally:
-            sys.stdout = original_stdout
+        self.uvsim.run()
+        self.assertIn('9999', output.getvalue())
+        sys.stdout = original_stdout
 
     def test_halt_success(self):
         program = [4300]
